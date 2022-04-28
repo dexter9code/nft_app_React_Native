991d16cd11b566c9071aa6ec92a545b192282436
rudy <doomdexter07@gmail.com>
2022-04-28 12:00:58 +0530

    created 4 app component

---
+++ app/components/NFTCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NFTCard(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default NFTCard;
---
+++ app/components/index.js
@@ -0,0 +1,3 @@
+export { default as AppStatusBar } from "./AppStatusBar";
+export { default as NFTCard } from "./NFTCard";
+export { default as HomeHeader } from "./HomeHeader";
